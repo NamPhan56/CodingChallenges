8b578716995f25e3e60602106e0b64479dbc16d2
NamPhan56 <namp@umass.edu>
2022-06-07 04:01:27 -0400

    saving for the night

--- JavaScript/secondHighest.js
+++ JavaScript/secondHighest.js
@@ -1,3 +1,38 @@
+/**Problem Statement
+ * Write a program that accepts sets of three numbers, and prints the second-maximum number among the three.
+ * 
+ * Input
+ * First line contains the number of triples, N.
+ * The next N lines which follow each have three space separated integers.
+ * 
+ * Output
+ * For each of the N triples, output one new line which contains the second-maximum integer among the three.
+ * 
+ * Constraints
+ * 1 ≤ N ≤ 6
+ * 1 ≤ every integer ≤ 10000
+ * The three integers in a single triplet are all distinct. That is, no two of them are equal.
+ * 
+ */
+function secondHighest(str){
+
+    list = str.split(' ');
 
 
-console.log('test');
+}
+
+var N = parseInt(readline()); //
+var input = [];
+for(let i=0; i<N;i++){
+    input.push(readline());
+}
+
+
+//code here
+
+//
+
+for(let print of input){
+    console.log(print);
+}
+
