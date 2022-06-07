64cca38f63942e5f61bddac4a86bed1755aa55fa
NamPhan56 <namp@umass.edu>
2022-06-07 14:41:07 -0400

    fixed some indentation with code

--- JavaScript/06062022/AddAllNumbers/src/AddAllNumbers.java
+++ JavaScript/06062022/AddAllNumbers/src/AddAllNumbers.java
@@ -6,35 +6,31 @@ import java.util.Scanner;
 public class AddAllNumbers {
 	public static void main(String[] args) throws FileNotFoundException {
 		 // pass the path to the file as a parameter
-		System.out.println(System.getProperty("java.class.path"));
-
-        //File file = new File(System.getProperty("user.dir")+"\\AddAllNumbers\\src\\input.txt");
+		//System.out.println(System.getProperty("java.class.path"));
 
 		File file = new File(System.getProperty("java.class.path")+"\\input.txt");
         AddAllNumbers indx = new AddAllNumbers();
         
         Scanner sc = new Scanner(file);
-			String T = sc.nextLine();
-	        ArrayList<String> input = new ArrayList<String>();
-	        
+		String T = sc.nextLine();
+		ArrayList<String> input = new ArrayList<String>();
+		
 //			String T = "1";
 //	        ArrayList<String> input = new ArrayList<String>();
 //	        input.add(T);
 //	        input.add("ab1231da");
 
+		//adds inputs to a list
+		for(int i=0; i<Integer.parseInt(T);i++){
+			input.add(sc.nextLine());
+		}
+		sc.close();
 
-	        //adds inputs to a list
-	        for(int i=0; i<Integer.parseInt(T);i++){
-	            input.add(sc.nextLine());
-	        }
-			sc.close();
-
-	        //goes through list and apply logic
-	        for(String print : input){
-	            //System.out.println(print);
-	            System.out.println(indx.addNumbersOnly(print));
-	        }
-
+		//goes through list and apply logic
+		for(String print : input){
+			//System.out.println(print);
+			System.out.println(indx.addNumbersOnly(print));
+		}
 	}
 
 	public int addNumbersOnly(String str){
